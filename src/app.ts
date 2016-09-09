1b58414783cdf68eb511c1fa6cd41a7709d904d3
Shyam Chen <shyamchen1994@gmail.com>
2016-09-09 11:45:43 +0800

    43rd Commit

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,11 @@ import * as express from 'express';
 const mongoose = require('mongoose');
 
 import * as bodyParser from 'body-parser';
+const cookieParser = require('cookie-parser');
 import * as logger from 'morgan';
+
+const passport = require('passport');
+const Strategy = require('passport-facebook').Strategy;
 
 import { route } from './routes';
 import { } from './models';
@@ -23,12 +27,53 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(logger('dev'));
 
 app.use(require('stylus').middleware(join(__dirname, 'public')));
 app.use(express.static(join(__dirname, 'public')));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(route);
+
+passport
+  .use(new Strategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/login/facebook/return'
+  },
+  (accessToken: any, refreshToken: any, profile: any, cb: any) => {
+    return cb(null, profile);
+  }));
+
+passport.serializeUser((user: any, cb: any) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj: any, cb: any) => {
+  cb(null, obj);
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/login/facebook', passport.authenticate('facebook'));
+
+app.get('/login/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  (req, res) => {
+    // res.render('profile', { user: req.user });
+  });
 
 app.use((req: any, res: any) => {
   res.status(404);
